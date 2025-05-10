a76951299801b0b21987d363d56edbfa8854e11a
harshit98 <harshitprasad28@gmail.com>
2025-05-10 20:58:43 +0530

    feat: add support for function_score clause in osquery package

---
+++ function_score.go
@@ -0,0 +1,104 @@
+// Package osquery Modified by harshit98 on 2025-05-10
+// Changes: Added function score support
+package osquery
+
+type FunctionScoreQuery struct {
+	query     Mappable
+	functions []Function
+	boostMode string
+	scoreMode string
+	maxBoost  float32
+	minScore  float32
+	boost     float32
+}
+
+type Function interface {
+	Map() map[string]interface{}
+}
+
+type RandomScoreFunction struct {
+	seed  *int64
+	field string
+}
+
+type FieldValueFactorFunction struct {
+	field    string
+	factor   float32
+	modifier string
+	missing  float32
+}
+
+// Additional functions can be added as per usecase in future like:
+// - ScriptScoreFunction
+// - DecayFunction (with variants for geo, date, numeric)
+// - WeightFunction
+//
+// For ref: https://docs.opensearch.org/docs/latest/query-dsl/compound/function-score/
+
+func FunctionScore(query Mappable) *FunctionScoreQuery {
+	return &FunctionScoreQuery{query: query}
+}
+
+func (q *FunctionScoreQuery) AddFunction(f Function) *FunctionScoreQuery {
+	q.functions = append(q.functions, f)
+	return q
+}
+
+func (q *FunctionScoreQuery) BoostMode(mode string) *FunctionScoreQuery {
+	q.boostMode = mode
+	return q
+}
+
+func (q *FunctionScoreQuery) Map() map[string]interface{} {
+	m := make(map[string]interface{})
+
+	if q.query != nil {
+		m["query"] = q.query.Map()
+	}
+
+	if len(q.functions) > 0 {
+		funcs := make([]map[string]interface{}, len(q.functions))
+		for i, f := range q.functions {
+			funcs[i] = f.Map()
+		}
+		m["functions"] = funcs
+	}
+
+	if q.boostMode != "" {
+		m["boost_mode"] = q.boostMode
+	}
+
+	return map[string]interface{}{
+		"function_score": m,
+	}
+}
+
+func RandomScore() *RandomScoreFunction {
+	return &RandomScoreFunction{}
+}
+
+func (f *RandomScoreFunction) Seed(seed int64) *RandomScoreFunction {
+	f.seed = &seed
+	return f
+}
+
+func (f *RandomScoreFunction) Field(field string) *RandomScoreFunction {
+	f.field = field
+	return f
+}
+
+func (f *RandomScoreFunction) Map() map[string]interface{} {
+	m := make(map[string]interface{})
+
+	if f.seed != nil {
+		m["seed"] = *f.seed
+	}
+
+	if f.field != "" {
+		m["field"] = f.field
+	}
+
+	return map[string]interface{}{
+		"random_score": m,
+	}
+}
---
+++ function_score_test.go
@@ -0,0 +1,176 @@
+package osquery
+
+import (
+	"testing"
+)
+
+func TestFunctionScore(t *testing.T) {
+	runMapTests(t, []mapTest{
+		{
+			"function_score query with random_score function",
+			FunctionScore(Term("user", "kimchy")).
+				AddFunction(RandomScore()),
+			map[string]interface{}{
+				"function_score": map[string]interface{}{
+					"query": map[string]interface{}{
+						"term": map[string]interface{}{
+							"user": map[string]interface{}{
+								"value": "kimchy",
+							},
+						},
+					},
+					"functions": []map[string]interface{}{
+						{
+							"random_score": map[string]interface{}{},
+						},
+					},
+				},
+			},
+		},
+		{
+			"function_score query with random_score function and boost_mode",
+			FunctionScore(Term("user", "kimchy")).
+				AddFunction(RandomScore()).
+				BoostMode("sum"),
+			map[string]interface{}{
+				"function_score": map[string]interface{}{
+					"query": map[string]interface{}{
+						"term": map[string]interface{}{
+							"user": map[string]interface{}{
+								"value": "kimchy",
+							},
+						},
+					},
+					"functions": []map[string]interface{}{
+						{
+							"random_score": map[string]interface{}{},
+						},
+					},
+					"boost_mode": "sum",
+				},
+			},
+		},
+		{
+			"function_score query with random_score function with seed",
+			FunctionScore(Term("user", "kimchy")).
+				AddFunction(RandomScore().Seed(42)),
+			map[string]interface{}{
+				"function_score": map[string]interface{}{
+					"query": map[string]interface{}{
+						"term": map[string]interface{}{
+							"user": map[string]interface{}{
+								"value": "kimchy",
+							},
+						},
+					},
+					"functions": []map[string]interface{}{
+						{
+							"random_score": map[string]interface{}{
+								"seed": int64(42),
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			"function_score query with random_score function with field",
+			FunctionScore(Term("user", "kimchy")).
+				AddFunction(RandomScore().Field("_seq_no")),
+			map[string]interface{}{
+				"function_score": map[string]interface{}{
+					"query": map[string]interface{}{
+						"term": map[string]interface{}{
+							"user": map[string]interface{}{
+								"value": "kimchy",
+							},
+						},
+					},
+					"functions": []map[string]interface{}{
+						{
+							"random_score": map[string]interface{}{
+								"field": "_seq_no",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			"function_score query with multiple functions",
+			FunctionScore(Term("user", "kimchy")).
+				AddFunction(RandomScore()).
+				AddFunction(RandomScore().Seed(123)),
+			map[string]interface{}{
+				"function_score": map[string]interface{}{
+					"query": map[string]interface{}{
+						"term": map[string]interface{}{
+							"user": map[string]interface{}{
+								"value": "kimchy",
+							},
+						},
+					},
+					"functions": []map[string]interface{}{
+						{
+							"random_score": map[string]interface{}{},
+						},
+						{
+							"random_score": map[string]interface{}{
+								"seed": int64(123),
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			"function_score query with match_all query",
+			FunctionScore(MatchAll()).
+				AddFunction(RandomScore()),
+			map[string]interface{}{
+				"function_score": map[string]interface{}{
+					"query": map[string]interface{}{
+						"match_all": map[string]interface{}{},
+					},
+					"functions": []map[string]interface{}{
+						{
+							"random_score": map[string]interface{}{},
+						},
+					},
+				},
+			},
+		},
+	})
+}
+
+func TestFunctionScoreWithQuery(t *testing.T) {
+	runMapTests(t, []mapTest{
+		{
+			"query with function_score",
+			Query(
+				FunctionScore(Term("user", "kimchy")).
+					AddFunction(RandomScore()).
+					BoostMode("sum"),
+			),
+			map[string]interface{}{
+				"query": map[string]interface{}{
+					"function_score": map[string]interface{}{
+						"query": map[string]interface{}{
+							"term": map[string]interface{}{
+								"user": map[string]interface{}{
+									"value": "kimchy",
+								},
+							},
+						},
+						"functions": []map[string]interface{}{
+							{
+								"random_score": map[string]interface{}{},
+							},
+						},
+						"boost_mode": "sum",
+					},
+				},
+			},
+		},
+	})
+}
